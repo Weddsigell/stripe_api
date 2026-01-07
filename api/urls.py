877a8edb284815040795f2e8fc0eedb4fafc19e4
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2026-01-08 00:38:20 +0500

    create urls

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import get_item, get_stripe_id
+
+app_name = "api"
+
+urlpatterns = [
+    path("item/<int:id>/", get_item),
+    path("buy/<int:id>/", get_stripe_id),
+]
