da10a3c0545a193233cf91820f4fda482f48ce07
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2026-01-08 00:37:11 +0500

    add stripe sk

--- stripe_api/settings.py
+++ stripe_api/settings.py
@@ -17,6 +17,10 @@ from environs import Env
 env = Env()
 env.read_env()
 
+
+# Stripe keys
+STRIPE_SECRET_KEY = env.str("STRIPE_SECRET_KEY")
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
