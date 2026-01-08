4979e73a133d7cb00b6656aaef4dae176c53fa78
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2026-01-08 20:06:25 +0500

    create get_item

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 import stripe
 from django.conf import settings
 from django.http import JsonResponse
-from django.shortcuts import redirect
+from django.shortcuts import render
 
 from .models import Item
 
@@ -10,7 +10,11 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 
 def get_item(request, id):
     item = Item.objects.get(id=id)
-    return item
+    return render(
+        request,
+        "item.html",
+        {"item": item, "publishable_key": settings.STRIPE_PUBLISHABLE_KEY},
+    )
 
 
 def get_stripe_session_id(request, id):
@@ -18,20 +22,21 @@ def get_stripe_session_id(request, id):
         item = Item.objects.get(id=id)
     except Item.DoesNotExist:
         return JsonResponse({"error": "Item does not exist"}, status=404)
-    
-    session = stripe.checkout.Session.create(
-        success_url="http://localhost:8000/success/",
-        line_items=[
-            {
-                "price_data": {
-                    "currency": "usd",
-                    "product_data": {"name": item.name},
-                    "unit_amount": item.price,
-                },
-                "quantity": 1,
-            }
-        ],
-        mode="payment",
-    )
 
-    return JsonResponse({"sessionId": session.id})
+    if request.method == "GET":
+        session = stripe.checkout.Session.create(
+            success_url="http://localhost:8000/success/",
+            line_items=[
+                {
+                    "price_data": {
+                        "currency": "usd",
+                        "product_data": {"name": item.name},
+                        "unit_amount": item.price,
+                    },
+                    "quantity": 1,
+                }
+            ],
+            mode="payment",
+        )
+
+        return JsonResponse({"session_id": session.id})
