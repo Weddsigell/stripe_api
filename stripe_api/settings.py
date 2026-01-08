1ff74dbecc071aa86f56d6af8f9c5a2a2c9f1f8d
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2026-01-09 04:09:08 +0500

    update allowed_hosts

--- stripe_api/settings.py
+++ stripe_api/settings.py
@@ -35,7 +35,7 @@ STRIPE_PUBLISHABLE_KEY = env.str("STRIPE_PUBLISHABLE_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
 
 
 # Application definition
