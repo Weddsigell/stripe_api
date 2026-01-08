8bdfd7ceeabdffb3a3227d829924f40e5fe97c58
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2026-01-08 19:09:57 +0500

    update urls

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import get_item, get_stripe_id
+from .views import get_item, get_stripe_session_id
 
 app_name = "api"
 
 urlpatterns = [
     path("item/<int:id>/", get_item),
-    path("buy/<int:id>/", get_stripe_id),
+    path("buy/<int:id>/", get_stripe_session_id),
 ]
