f595b431c0ebeb4fc676df458c7c22ad303512bc
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2026-01-09 05:01:07 +0500

    update price in item

--- api/models.py
+++ api/models.py
@@ -1,10 +1,14 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
 class Item(models.Model):
     name = models.CharField(max_length=255, null=False)
     discription = models.TextField(null=False)
-    price = models.PositiveIntegerField()
+    price = models.PositiveIntegerField(
+        validators=[MinValueValidator(51)],
+        help_text="Цена в центах (минимум 51 = $0.51)",
+    )
 
     class Meta:
         verbose_name = "Item"
